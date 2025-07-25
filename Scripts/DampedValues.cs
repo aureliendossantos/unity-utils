4f17ccab1f607bad7870e8e60cc6e2c7d066054b
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2025-07-25 22:07:00 +0200

    fix: syntax is too modern

--- Scripts/DampedValues.cs
+++ Scripts/DampedValues.cs
@@ -5,16 +5,22 @@ using NaughtyAttributes;
 /// Use instead of a simple `float` to have a smooth transition towards a target value that can be changed at any time.
 /// </summary>
 [System.Serializable]
-public class DampedFloat(float initialValue)
+public class DampedFloat
 {
-    [ShowNativeProperty] public float Value { get; private set; } = initialValue;
-    [HideInInspector] public float Target = initialValue;
+    [ShowNativeProperty] public float Value { get; private set; }
+    [HideInInspector] public float target;
     float velocity = 0f;
     [SerializeField] float smoothTime = 0.5f;
 
+    public DampedFloat(float initialValue)
+    {
+        Value = initialValue;
+        target = initialValue;
+    }
+
     public void Update()
     {
-        Value = Mathf.SmoothDamp(Value, Target, ref velocity, smoothTime);
+        Value = Mathf.SmoothDamp(Value, target, ref velocity, smoothTime);
     }
 }
 
@@ -22,15 +28,21 @@ public class DampedFloat(float initialValue)
 /// Works like `DampedFloat` with a smoothing function that is more suitable for angles.
 /// </summary>
 [System.Serializable]
-public class DampedAngle(float initialValue)
+public class DampedAngle
 {
-    [ShowNativeProperty] public float Value { get; private set; } = initialValue;
-    [HideInInspector] public float Target = initialValue;
+    [ShowNativeProperty] public float Value { get; private set; }
+    [HideInInspector] public float target;
     float velocity = 0f;
     [SerializeField] public float smoothTime = 0.5f;
 
+    public DampedAngle(float initialValue)
+    {
+        Value = initialValue;
+        target = initialValue;
+    }
+
     public void Update()
     {
-        Value = Mathf.SmoothDampAngle(Value, Target, ref velocity, smoothTime);
+        Value = Mathf.SmoothDampAngle(Value, target, ref velocity, smoothTime);
     }
 }
